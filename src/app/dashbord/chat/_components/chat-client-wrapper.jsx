58ca1a121efd34c0fcead3f93ac75f077293cae0
Amr-Guaily <amrguaily@gmail.com>
2025-04-12 12:54:28 +0200

    Refactor ChatPage to use ChatClientWrapper for rendering chat history and message input

---
+++ src/app/dashbord/chat/_components/chat-client-wrapper.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import SendNewMessageForm from "../_components/send-new-message";
+import ChatHistory from "../_components/chat-history";
+
+import { useState } from "react";
+
+export default function ChatClientWrapper({ initialData, userId }) {
+  const [chatHistory, setChatHistory] = useState(initialData);
+
+  const OnNewMessage = (newMessage) => {
+    setChatHistory((prev) => [...prev, newMessage]);
+  };
+
+  return (
+    <div className="relative container mx-auto flex flex-col h-full bg-gray-200 rounded-md pb-14">
+      <ChatHistory data={chatHistory} />
+
+      {/* Render the form and notify ChatHistory of new messages */}
+      <SendNewMessageForm userId={userId} OnNewMessage={OnNewMessage} />
+    </div>
+  );
+}
