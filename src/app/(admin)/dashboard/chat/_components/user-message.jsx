f344f2424cf1fd8772f9ddbdebd1626bc7346adf
Amr Guaily <77831717+Amr-Guaily@users.noreply.github.com>
2025-04-22 17:41:17 +0200

    Refactor UserMessageCard styles for improved readability (#170)

--- src/app/(admin)/dashboard/chat/_components/user-message.jsx
+++ src/app/(admin)/dashboard/chat/_components/user-message.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function UserMessageCard({ message }) {
   return (
-    <div className="w-fit rounded-lg p-2 bg-primary text-white flex flex-col">
-      <div className="text-sm">{message}</div>
-      <div className="text-xs text-gray-200 mt-2">
+    <div className="w-fit rounded-lg p-2 bg-[#e2dbff] text-white flex flex-col">
+      <div className="text-sm text-black">{message}</div>
+      <div className="text-xs mt-2 text-black">
         {new Date().toLocaleString()}
       </div>
     </div>
