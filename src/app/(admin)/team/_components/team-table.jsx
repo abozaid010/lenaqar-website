48292e316fafafd33c5e6fc977a6c74d07bb76e3
Amr-Guaily <amrguaily@gmail.com>
2025-05-17 13:44:45 +0300

    ✨ refactor TeamPage to utilize TeamTable component and remove console logs

---
+++ src/app/(admin)/team/_components/team-table.jsx
@@ -0,0 +1,11 @@
+export default function TeamTable({ data }) {
+  return (
+    <div className="border border-gray-200 sm:rounded-lg scroll-snap-x-mandatory ">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-100"></thead>
+
+        <tbody className="bg-white divide-y divide-gray-200"></tbody>
+      </table>
+    </div>
+  );
+}
--- src/app/(admin)/team/page.jsx
+++ src/app/(admin)/team/page.jsx
@@ -1,25 +1,17 @@
 import { getSalesData } from "@/components/services/serviceFetching";
 import AddNewMember from "./_components/add-new-member";
 import { getClientid } from "@/components/services/clientCookies";
+import TeamTable from "./_components/team-table";
 
 export default async function TeamPage() {
   const clientId = await getClientid();
   const data = await getSalesData();
 
-  console.log("Client ID:", clientId);
-  console.log("Sales Data:", data);
-
   return (
     <div className="container mx-auto">
       <AddNewMember clientId={clientId} />
 
-      <div className="border border-gray-200 sm:rounded-lg scroll-snap-x-mandatory ">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-100"></thead>
-
-          <tbody className="bg-white divide-y divide-gray-200"></tbody>
-        </table>
-      </div>
+      <TeamTable data={data} />
     </div>
   );
 }
