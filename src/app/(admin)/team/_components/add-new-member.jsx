a9f050048f58ff23f128ac86345e853772d86148
Amr-Guaily <amrguaily@gmail.com>
2025-05-17 13:39:27 +0300

    ✨ implement AddNewMember component and update TeamPage layout

---
+++ src/app/(admin)/team/_components/add-new-member.jsx
@@ -0,0 +1,8 @@
+export default function AddNewMember() {
+  return (
+    <div className="container mx-auto">
+      <h1>Add New Member</h1>
+      <p>This is the add new member page.</p>
+    </div>
+  );
+}
--- src/app/(admin)/team/page.jsx
+++ src/app/(admin)/team/page.jsx
@@ -1,8 +1,20 @@
-export default function TeamPage() {
+import AddNewMember from "./_components/add-new-member";
+import { getClientId } from "@/lib/auth";
+
+export default async function TeamPage() {
+  const clientId = await getClientId();
+
   return (
-    <div>
-      <h1>Team Page</h1>
-      <p>This is the team page.</p>
+    <div className="container mx-auto">
+      <AddNewMember />
+
+      <div className="border border-gray-200 sm:rounded-lg scroll-snap-x-mandatory ">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-100"></thead>
+
+          <tbody className="bg-white divide-y divide-gray-200"></tbody>
+        </table>
+      </div>
     </div>
   );
 }
