c87f0ecfadfaedd865865264fd5e557cc090f0c5
Amr-Guaily <amrguaily@gmail.com>
2025-05-17 16:12:42 +0300

    ✨ update import statement for Dialog component to match file naming convention

--- src/app/(admin)/units/_components/add-developer-dialog.jsx
+++ src/app/(admin)/units/_components/add-developer-dialog.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Dialog from "@/components/ui/dialog";
+import Dialog from "@/components/ui/Dialog";
 import { Loader2 } from "lucide-react";
 import { addDeveloper } from "@/components/services/serviceFetching";
 import { v4 as uuidv4 } from "uuid";
