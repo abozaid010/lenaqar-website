ea4d86db25c8e4e41fb108c0e6f1dfc7924f25aa
Amr-Guaily <amrguaily@gmail.com>
2025-05-17 11:41:58 +0300

    ✨ pass clientID to Header component for improved user context

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -7,6 +7,7 @@ import { cookies } from "next/headers";
 const Layout = async ({ children }) => {
   // Get the clientID from the cookie on the server then pass it as a prop to the Header component => To avoid hydration issues
   const cookieStore = await cookies();
+  const clientID = cookieStore.get("client_id")?.value;
   const clientName = JSON.parse(
     cookieStore.get("client_info")?.value
   )?.client_name;
@@ -25,7 +26,7 @@ const Layout = async ({ children }) => {
         <Sidebar />
 
         <div className="flex-1 flex flex-col overflow-hidden lg:pl-0">
-          <Header clientName={clientName} />
+          <Header clientName={clientName} clientID={clientID} />
 
           <main className="overflow-y-auto p-3 relative flex-1">
             {children}
