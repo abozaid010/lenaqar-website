6df553d9a62729e8054356f4ba157ac94b783ccf
Amr-Guaily <amrguaily@gmail.com>
2025-05-17 12:51:06 +0300

    ✨ remove disabled and required attributes from code input in BasicDetailsStep

--- src/components/ui/unit-details/unit-basic-info.jsx
+++ src/components/ui/unit-details/unit-basic-info.jsx
@@ -16,6 +16,12 @@ export default function UnitBasicInfo({ unit }) {
 
       <h1 className="mt-2 text-3xl font-bold text-primary">{unit.unitTitle}</h1>
 
+      {unit.code && (
+        <p className="text-lg text-gray-600">
+          unit code: <span className="font-semibold">{unit.code}</span>
+        </p>
+      )}
+
       <p className="text-lg text-gray-600">
         {unit.project}, {unit.city}
       </p>
