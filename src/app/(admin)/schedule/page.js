ddeb1be80c0242262b68b36e31407e93b3271d34
hissine hamada <105513061+hissine72@users.noreply.github.com>
2025-06-03 16:12:43 +0300

    issue build (#357)

--- src/app/(admin)/schedule/page.js
+++ src/app/(admin)/schedule/page.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import Schedual from './components/Schedual'
 import { getschedual } from '@/components/services/serviceFetching';
+
+export const dynamic = 'force-dynamic';
+
 const today = new Date();
-today.setHours(0, 0, 0, 0); 
+today.setHours(0, 0, 0, 0); // نزيل الوقت
 
 const sevenDaysAgo = new Date(today);
 sevenDaysAgo.setDate(today.getDate() - 7);
 
 const formattedBefore = sevenDaysAgo.toISOString().split('T')[0];
-
+console.log("تاريخ قبل 7 أيام هو:", formattedBefore);
 // const today = new Date();
-today.setHours(0, 0, 0, 0);
+today.setHours(0, 0, 0, 0); // نزيل الوقت
 
 const sevenDaysLater = new Date(today);
 sevenDaysLater.setDate(today.getDate() + 7);
 
 const formattedAfter = sevenDaysLater.toISOString().split('T')[0];
-
+console.log("تاريخ بعد 7 أيام هو:", formattedAfter);
 
 
 const page =  async () => {
