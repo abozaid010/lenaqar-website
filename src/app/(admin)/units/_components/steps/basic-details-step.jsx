44de7f90d774a6ce2d6c2e144856d03471c92818
Amr-Guaily <amrguaily@gmail.com>
2025-06-19 13:12:28 +0300

    refactor: replace CitySelect with sorted-city-select for improved city sorting

--- src/app/(admin)/units/_components/steps/basic-details-step.jsx
+++ src/app/(admin)/units/_components/steps/basic-details-step.jsx
@@ -3,9 +3,9 @@
 import { getprojects } from "@/components/services/serviceFetching";
 import AddCompoundDialog from "@/components/ui/add-compound-dialog";
 import AddPhaseDialog from "@/components/ui/add-phase-dialog";
-import CitySelect from "@/components/ui/inputs/city-select";
 import FormInput from "@/components/ui/inputs/form-input";
 import FormSelect from "@/components/ui/inputs/form-select";
+import CitySelect from "@/components/ui/inputs/sorted-city-select";
 import { useI18n } from "@/context/translate-api";
 import {
   convertArabicToEnglishNumbers,
