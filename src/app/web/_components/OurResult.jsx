e986c9e9ea6b41bedfb8d9b386bc42e17c001f94
Amr-Guaily <amrguaily@gmail.com>
2025-06-22 13:34:16 +0300

    refactor: simplify layout and improve styling in OurResult component

--- src/app/web/_components/OurResult.jsx
+++ src/app/web/_components/OurResult.jsx
@@ -1,14 +1,8 @@
 "use client";
 import { useI18n } from "@/context/translate-api";
 
-const toArabicDigits = (str) => {
-  // يحول كل رقم إنجليزي إلى رقمه العربي
-  return str.replace(/\d/g, (d) => "٠١٢٣٤٥٦٧٨٩"[d]);
-};
-
 const OurResult = () => {
   const { t } = useI18n();
-  const isRTL = t.direction === "rtl";
   const results = [
     {
       value: t.ourResultsInNumbers.fasterLeadResponseValue,
@@ -35,24 +29,22 @@ const OurResult = () => {
       label: t.ourResultsInNumbers.increaseInCSAT,
     },
   ];
+
   return (
     <div className="bg-[#fafbfc] py-16" dir={t.direction}>
       <h2 className="text-center text-4xl font-bold text-gray-800 mb-12">
         {t.ourResultsInNumbers.title}
       </h2>
-      <div
-        className={`flex flex-col md:flex-row ${isRTL ? "md:flex-row-reverse" : ""} justify-center md:gap-x-6 gap-y-6 md:gap-y-0 w-full container`}
-      >
+      <div className="flex justify-center gap-2 w-full container flex-wrap">
         {results.map((item, idx) => (
           <div
             key={idx}
-            className="bg-white rounded-xl shadow-sm border border-gray-200 px-8 py-6 min-w-[200px] text-center flex flex-col items-center flex-1"
+            className="bg-white rounded-xl shadow-sm border border-gray-200 px-2 py-5 min-w-[160px] text-center flex flex-col items-center gap-2 flex-1"
           >
-            <div className="text-3xl font-bold mb-2 bg-gradient-to-r from-[#366cd9] to-[#3ec6e0] bg-clip-text text-transparent">
-              {/* {isRTL ? toArabicDigits(item.value) : item.value} */}
+            <div className="text-2xl font-bold bg-gradient-to-r from-[#366cd9] to-[#3ec6e0] bg-clip-text text-transparent">
               {item.value}
             </div>
-            <div className="text-lg text-gray-600 font-normal leading-snug">
+            <div className="text-base text-gray-600 font-normal leading-snug">
               {item.label}
             </div>
           </div>
