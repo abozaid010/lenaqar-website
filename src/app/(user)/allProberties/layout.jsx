527b8280ee7905a3eddc3ce9aa292fd3dc33267d
Amr-Guaily <amrguaily@gmail.com>
2025-06-22 10:11:33 +0300

    refactor: update Header and layout components for improved structure and styling; enhance container padding in globals.css

--- src/app/(user)/allProberties/layout.jsx
+++ src/app/(user)/allProberties/layout.jsx
@@ -2,10 +2,10 @@ import Header from "./_components/Header";
 
 const layout = ({ children }) => {
   return (
-    <div>
+    <>
       <Header />
       <main className="mt-20">{children}</main>
-    </div>
+    </>
   );
 };
 
