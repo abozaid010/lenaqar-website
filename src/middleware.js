09901905d71ab37fa53ab7a7ab4ae38cb9241de2
Amr-Guaily <amrguaily@gmail.com>
2025-07-01 19:24:14 +0300

    feat: enhance middleware to require both access and refresh tokens for protected routes

--- src/middleware.js
+++ src/middleware.js
@@ -2,16 +2,18 @@ import { NextResponse } from "next/server";
 
 const protectedRoutes = ["/dashboard", "/units", "/team", "/client"];
 
-export async function middleware(request) {
+export async function middleware(request, response) {
   const path = request.nextUrl.pathname;
 
   const accessToken = request.cookies.get("access_token")?.value;
+  const refreshToken = request.cookies.get("refresh_token")?.value;
+
   const isProtectedRoute = protectedRoutes.some((route) =>
     path.startsWith(route)
   );
 
   if (isProtectedRoute) {
-    if (!accessToken) {
+    if (!accessToken || !refreshToken) {
       const response = NextResponse.redirect(new URL("/login", request.url));
       response.cookies.delete("access_token");
       response.cookies.delete("refresh_token");
