6270888076f9eef6bd69ebd233b077eb4b97c568
Amr-Guaily <amrguaily@gmail.com>
2025-07-05 15:13:40 +0300

    fix: update client ID retrieval and improve API request structure

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,18 +4,18 @@ import { axiosInstance } from "@/lib/axiosInstance";
 import Cookies from "js-cookie";
 
 export async function fetchUsersData(searchParams) {
-  const clientId = Cookies.get("client_id");
+  const clientId = Cookies.get("lena-website-client_id");
 
+  console.log("Client ID:", clientId);
   try {
     const params = {
       ...JSON.parse(searchParams),
       limit: 20,
     };
 
-    console.log("Fetching users with params:", params);
-
-    console.log("Fetching users for client:", clientId);
-    const response = await axiosInstance.get(`dashboard/amr`, { params });
+    const response = await axiosInstance.get(`dashboard/${clientId}`, {
+      params,
+    });
     return response.data;
   } catch (error) {
     console.error("Failed to fetch users:", error.message);
