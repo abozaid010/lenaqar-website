0927bbc2023303391dedc37ee09ec384e574a8b0
Amr-Guaily <amrguaily@gmail.com>
2025-07-05 13:26:08 +0300

    feat: add @tanstack/react-query for improved data fetching and state management refactor: update layout to include TanStackQueryProvider for query context refactor: simplify client data fetching in api.js and remove unused code in clients-table

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,21 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axiosInstance";
+import Cookies from "js-cookie";
 
 export async function fetchUsersData(searchParams) {
-  const clientId = "amr";
+  const clientId = Cookies.get("client_id");
 
   try {
-    // const params = {
-    //   ...JSON.parse(searchParams),
-    //   limit: 20,
-    // };
+    const params = {
+      ...JSON.parse(searchParams),
+      limit: 20,
+    };
 
-    // console.log("Fetching users with params:", params);
+    console.log("Fetching users with params:", params);
 
     console.log("Fetching users for client:", clientId);
-    const response = await axiosInstance.get(`dashboard/amr`);
-
+    const response = await axiosInstance.get(`dashboard/amr`, { params });
     return response.data;
   } catch (error) {
     console.error("Failed to fetch users:", error.message);
