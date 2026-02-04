a80d1a9a8a70b5446f192042ec8c5ab3038ce232
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2026-02-04 20:07:06 +0200

    asdfasd

--- src/components/ui/inputs/image-uploader.jsx
+++ src/components/ui/inputs/image-uploader.jsx
@@ -298,7 +298,7 @@ export default function ImageUploader({
                   removeUploadedImage(imageId);
                 }
               }}
-              className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1 opacity-0 group-hover:opacity-100 transition-opacity"
+              className="absolute top-1 right-1 h-6 bg-red-500 text-white rounded-full p-1 opacity-0 group-hover:opacity-100 transition-opacity"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
