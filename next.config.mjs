61c1e9918e74570696aebe2e95a292048c9db21c
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2026-02-04 21:27:51 +0200

    Initail commit

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const imageHostname = (() => {
   }
 })();
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -55,7 +56,7 @@ const nextConfig = {
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
     // Improve image loading performance
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 60*60*8, // 8 hours
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
