a0501eb0be6a4115afaa6e93ab4b8702c000e19c
abozaid010 <abozaid.ibrahim11@gmail.com>
2026-02-07 13:56:25 +0200

    no dimssi payment

--- src/components/ui/add-payment-plan-dialog.jsx
+++ src/components/ui/add-payment-plan-dialog.jsx
@@ -264,6 +264,7 @@ export default function AddPaymentPlanDialog({
     <Dialog
       isOpen={isOpen}
       onClose={onClose}
+      closeOnOutsideClick={false}
       title={
         editMode
           ? t.modal.updatePaymentPlan || "Edit Payment Plan"
